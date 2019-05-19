38e7b50facb73f4490bda04f14207121441cc732
ykhs <yukihisa.yamada@gmail.com>
2019-05-20 01:44:15 +0900

    chore(2-validate_id_token): Copy from examples/0-login.

---
+++ examples/2-validate_id_token/src/views/Callback.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="callback"></div>
+</template>
+
+<script>
+export default {
+  name: 'callback',
+  created() {
+    this.$store.dispatch('handleAuthentication');
+    this.$router.replace('/');
+  },
+};
+</script>
